825a8ba58033f5b597a7b877dd64fb8aa4ab2fa5
tnsdo <jungeun2115@gmail.com>
2024-08-23 14:18:17 +0900

    board 디자인

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,83 @@
 import "./App.css";
 
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
-import styled, { CSSProperties } from "styled-components";
+import styled from "styled-components";
 
-import ReactLogo from "./assets/react.svg?react";
+import { Data, mockData } from "./mockData";
 
-const ReadTheDocs = styled.p<{
-  $color?: CSSProperties["color"];
-}>`
-  color: ${({ $color }) => $color ?? "red"};
+const Display = styled.div`
+  width: 600px;
+  height: 100vh;
 `;
 
-function App() {
-  const [count, setCount] = useState(0);
-  const { t } = useTranslation("main");
+const Header = styled.div`
+  font-family: "Goblin One";
+  text-align: center;
+  font-size: 40px;
+  font-weight: 400;
+  line-height: 50px;
+  color: black;
+`;
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src="/src/assets/react.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <ReadTheDocs $color={"blue"}>
-        {t("title")}
-        <ReactLogo width={16} height={16} />
-      </ReadTheDocs>
-    </div>
-  );
+interface ShowDataProps {
+  data: Data;
 }
+
+const BoardList = styled.ul`
+  align-items: center;
+  list-style: none;
+  margin-top: 30px;
+`;
+
+const BoardItem = styled.li`
+  background-color: #f4f4f4;
+  height: 80px;
+  width: 500px;
+  margin-bottom: 10px;
+  border-radius: 15px;
+  align-items: flex-start;
+  flex-direction: column;
+`;
+
+const BoardTitle = styled.div`
+  font-family: "Pretendard";
+  font-size: 26px;
+  font-weight: 600;
+  line-height: 31.03px;
+  text-align: left;
+  color: black;
+  margin-left: 20px;
+  padding-top: 13px;
+`;
+
+const BoardBody = styled.div`
+  font-family: "Pretendard";
+  font-size: 18px;
+  text-align: left;
+  margin-left: 20px;
+`;
+
+interface ShowDataProps {
+  data: Data;
+}
+
+const ShowData: React.FC<ShowDataProps> = ({ data }) => (
+  <BoardItem>
+    <BoardTitle>{data.title}</BoardTitle>
+    <BoardBody>{data.body}</BoardBody>
+  </BoardItem>
+);
+
+const App = () => {
+  return (
+    <Display>
+      <Header>Board</Header>
+      <BoardList>
+        {mockData.map((post) => (
+          <ShowData key={post.id} data={post} />
+        ))}
+      </BoardList>
+    </Display>
+  );
+};
 
 export default App;
